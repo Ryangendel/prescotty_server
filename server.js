07a9b44c28f1e7a427d5847e83c12fbb348582c9
Ryan <ryan.gendel@bluelabellabs.com>
2023-08-26 16:55:52 -0600

    all 4 pos systems working

--- server.js
+++ server.js
@@ -34,22 +34,17 @@ app.post('/submit', ({ body }, res) => {
             res.json(err);
         });
 });
-
+var count = 0
 app.get('/all', (req, res) => {
     var results = []
     fs.createReadStream('./contacts_live.csv')
         .pipe(csv())
         .on('data', async (data) => {
-            // console.log("************************************")
-            // console.log(i)
-            // console.log("************************************")
-            // var formattedData = []
+
             var dataObj = {}
-            var dataArr = [data]
-            // console.log("-------------")
-            // console.log(data)
-            // console.log("-------------")
-            // for (let i = 0; i < dataArr.length; i++) {
+            //LOOOP OF DATA OBJECTS
+
+
             dataObj.full_name = data.Name
             dataObj.mobile = data["Phone 1 - Value"]
             dataObj.address1 = data["Address 1 - Street"]
@@ -57,164 +52,204 @@ app.get('/all', (req, res) => {
             dataObj.state = data["Address 1 - Region"]
             dataObj.zip = data["Address 1 - Postal Code"]
             const dobRegex = /DOB: (\d{2}\/\d{2}\/\d{4})/;
-         
-
             const dob = data.Notes.match(dobRegex);
             if (dob && dob[1]) {
                 dataObj.dob = dob[1];
             }
             const taskIdRegex = /Onfleet Task ID:\s+(\w+)/;
-            const taskId = data.Notes.match(taskIdRegex);
-            if (taskId && taskId[1]) {
-                dataObj.task_id = taskId[1];
-            }
-            const createdAtRegex = /created at (\d+)/;
-            const order_time = data.Notes.match(createdAtRegex);
-            if (order_time && order_time[1]) {
-                const createdAtTime = new Date(parseInt(order_time[1]));
-                dataObj.order_date = createdAtTime;
-            }
-            const viewOrderRegex = /View order: (https:\/\/[^\s]+)/;
-            const viewOrder = data.Notes.match(viewOrderRegex);
-            if (viewOrder && viewOrder[1]) {
-                const viewOrderLink = viewOrder[1];
-                dataObj.view_order = viewOrderLink;
-            }
 
-            const orderNumberRegex = /Order Number: (\d+)/;
-            const OrderNumber = data.Notes.match(orderNumberRegex);
-            if (OrderNumber && OrderNumber[1]) {
-                const orderNumber = OrderNumber[1];
-                dataObj.order_number = orderNumber;
-            }
-            const productRegex = /([A-Za-z\s]+)\nOption: [^\n]+\nQuantity: \d+\nBrand: [^\n]+\nPrice: \d+/g;
-            const ProductName = data.Notes.match(productRegex);
+            //IF DUTCHIE ORDER ---------------------------------------------       
+            if (data.Notes.includes("https://dhcie")) {
+                dataObj.pos_system_used = "Dutchie"
+                const taskId = data.Notes.match(taskIdRegex);
+                if (taskId && taskId[1]) {
+                    dataObj.task_id = taskId[1];
+                }
+                const createdAtRegex = /created at (\d+)/;
+                const order_time = data.Notes.match(createdAtRegex);
+                if (order_time && order_time[1]) {
+                    const createdAtTime = new Date(parseInt(order_time[1]));
+                    dataObj.order_date = createdAtTime;
+                }
+                const viewOrderRegex = /View order: (https:\/\/[^\s]+)/;
+                const viewOrder = data.Notes.match(viewOrderRegex);
+                if (viewOrder && viewOrder[1]) {
+                    const viewOrderLink = viewOrder[1];
+                    dataObj.view_order = viewOrderLink;
+                }
 
-            if (ProductName) {
-                const productNames = ProductName.map(match => match.trim());
-                dataObj.product_info = productNames
-            }
+                const orderNumberRegex = /Order Number: (\d+)/;
+                const OrderNumber = data.Notes.match(orderNumberRegex);
+                if (OrderNumber && OrderNumber[1]) {
+                    const orderNumber = OrderNumber[1];
+                    dataObj.order_number = orderNumber;
+                }
+                const productRegex = /([A-Za-z\s]+)\nOption: [^\n]+\nQuantity: \d+\nBrand: [^\n]+\nPrice: \d+/g;
+                const ProductName = data.Notes.match(productRegex);
 
-            const costRegex = /(\w+): (\$[\d.]+)/g;
+                if (ProductName) {
+                    const productNames = ProductName.map(match => match.trim());
+                    dataObj.product_info = productNames
+                }
 
-            const matches = data.Notes.matchAll(costRegex);
+                const costRegex = /(\w+): (\$[\d.]+)/g;
 
-            for (const match of matches) {
-                const key = match[1];
-                const value = match[2];
-                dataObj[key] = value;
-            }
-            //---------------------------------WORKING CODE
-            // const itemRegex = /Option: ([^\n]+)\nQuantity: (\d+)\nBrand: ([^\n]+)\nPrice: (\d+)/g;
+                const matches = data.Notes.matchAll(costRegex);
 
-            // const extractedItems = [];
-            // let match;
+                for (const match of matches) {
+                    const key = match[1];
+                    const value = match[2];
+                    dataObj[key] = value;
+                }
 
-            // while ((match = itemRegex.exec(data.Notes)) !== null) {
-            //     const [, y, option, quantity, brand, price] = match;
-            //     console.log("888888888888888888888888======================")
-            //     console.log(y)
-            //     console.log(option)
-            //     console.log(quantity)
-            //     console.log(brand)
-            //     console.log(price)
-            //     console.log("888888888888888888888888======================")
-            //     extractedItems.push({
-            //         itemPurchased: "N/A",
-            //         option,
-            //         quantity: parseInt(quantity),
-            //         brand,
-            //         price: parseFloat(price)
-            //     });
-            // }
-           // ---------------------------- WORKING CODE
-            //5555555555555555555555555555555555555555555555
                 var orders = dataObj.product_info
-              
-            if (orders?.length) {
-                console.log("----------------------------99900000---------============")  
-                console.log(orders.length)
-                console.log("---------------------------- 9999999  ---------============")  
-                // console.log("----------------------------   ---------============")  
-                //  console.log(typeof orders)
-                //  console.log("----------------------------   ---------============")  
-                var extractedItems = [];
-                orders.forEach(entry => {
-                    const lines = entry.split('\n');
-                    const [productNameLine, optionLine, quantityLine, brandLine, priceLine] = lines;
-                    
-                    const productName = productNameLine.split('\n')[0];
-                    const option = optionLine.split(": ")[1];
-                    const quantity = quantityLine.split(": ")[1];
-                    const brand = brandLine.split(": ")[1];
-                    const price = priceLine.split(": ")[1];
-                  
-                    extractedItems.push({
-                      product_name: productName,
-                      option,
-                      quantity: parseInt(quantity),
-                      brand,
-                      price: parseFloat(price)
+                //r65253f%@reafqertr43ret235f4/#QR#$R$#F#$F#qcfewqrfeqw
+                if (orders?.length) {
+                    var extractedItems = [];
+                    orders.forEach(entry => {
+                        const lines = entry.split('\n');
+                        const [productNameLine, optionLine, quantityLine, brandLine, priceLine] = lines;
+                        const productName = productNameLine.split('\n')[0];
+                        const option = optionLine.split(": ")[1];
+                        const quantity = quantityLine.split(": ")[1];
+                        const brand = brandLine.split(": ")[1];
+                        const price = priceLine.split(": ")[1];
+
+                        extractedItems.push({
+                            product_name: productName,
+                            option,
+                            quantity: parseInt(quantity),
+                            brand,
+                            price: parseFloat(price)
+                        });
                     });
-                  });
+                }
+                dataObj.pos_system_used = "Dutchie"
+                dataObj.purchase_details = extractedItems
             }
-            //5555555555555555555555555555555555555555555555
-            //     console.log("----------------------------   ---------============")  
-            //     console.log(ProductName[0])
-            //     console.log("----------------------------   ---------============")  
-            //     ProductName[0]?.forEach(all =>{ 
-            //     const entries = all.split('",\n"');
 
-                // const extractedItems = [];
+            //END DUTCHIE ORDER------------------------------
+            //START LEAFLY ORDERS
+            if (data.Notes.includes("Leafly")) {
+                // Function to extract purchase details
+                function extractOrderInfo(inputString) {
+                    const taskIDPattern = /Onfleet Task ID:\s+([^\n]+)/;
+                    const timeStampPattern = /created at (\d+)/;
+                    const orderIDPattern = /Leafly Order ID:\s+(\d+)/;
+                    const orderTotalPattern = /Order Total \(tax incl.\):\s+([\d.]+)/;
 
-                // entries.forEach(entry => {
-                //     const lines = entry.split('\n');
-                //     const productName = lines[0];
-                //     const option = lines[1].split(": ")[1];
-                //     const quantity = lines[2].split(": ")[1];
-                //     const brand = lines[3].split(": ")[1];
-                //     const price = lines[4].split(": ")[1];
+                    const extractedOrder = {};
+                    const taskIDMatch = inputString.match(taskIDPattern);
 
-                //     extractedItems.push({
-                //         product_name: productName,
-                //         option,
-                //         quantity: parseInt(quantity),
-                //         brand,
-                //         price: parseFloat(price)
-                //     });
-                // });
-           // })
-            // console.log("----------------------------===============")
-            // console.log(extractedItems)
-            // console.log("----------------------------===============")
-            dataObj.purchase_details = extractedItems
-            // dataObj.notes = data.Notes
+                    if (taskIDMatch) {
+                        // Remove "created at" from task_id
+                        extractedOrder.task_id = taskIDMatch[1].replace(/ created at \d+/, '').trim();
+                    }
+
+                    extractedOrder.time_stamp = parseInt(inputString.match(timeStampPattern)[1]);
+                    extractedOrder.order_id = inputString.match(orderIDPattern)[1].trim();
+                    extractedOrder.total = parseFloat(inputString.match(orderTotalPattern)[1]);
+
+                    return extractedOrder;
+                }
+
+                const extractedData1 = extractOrderInfo(data.Notes);
+
+                dataObj.task_id = extractedData1.task_id
+
+                //new Date(parseInt(inputString.match(timePattern)[1])).toISOString();
+                dataObj.time_stamp = new Date(extractedData1.time_stamp)
+                dataObj.order_id = extractedData1.order_id
+                dataObj.total = extractedData1.total
+                dataObj.pos_system_used = "Leafly"
+            }
+
+            //END LEAFLY ORDERS---------------------------------
+            //START Transaction ID --- NOT SURE THE POS
+            if (data.Notes.includes("Transaction ID")) {
+                // console.log(data.Notes)
+                //******************************************** */
+                function extractInfo(line) {
+                    const regex = /([\d.]+) x (.+?) ([\d.]+(?: Gram|g)) .+?\| (\d+)/;
+                    const match = line.match(regex);
+                    if (match) {
+                        const quantity = parseFloat(match[1]);
+                        const product = match[2];
+                        const option = match[3];
+                        const sku = parseInt(match[4]);
+                        return { quantity, product, option, sku };
+                    }
+                    return null;
+                }
+
+                // Function to extract Total and Sub-Total
+                function extractTotals(lines) {
+                    const subTotalLine = lines.find(line => line.includes('Sub-Total:'));
+                    const totalLine = lines.find(line => line.includes('Total:'));
+                    const subTotal = parseFloat(subTotalLine.match(/\$([\d.]+)/)[1]);
+                    const total = parseFloat(totalLine.match(/\$([\d.]+)/)[1]);
+                    return { subTotal, total };
+                }
+
+                // Split the input string into lines
+                const lines1 = data.Notes.split('\n');
+
+                // Extract information and totals
+                const info1 = lines1.map(extractInfo).filter(Boolean);
+                const totals1 = extractTotals(lines1);
+
+                dataObj.pos_system_used = "not sure1"
+                dataObj.order = info1
+                dataObj.subtotal = totals1.subTotal
+                dataObj.total = totals1.total
+            }
+            //END Transaction ID --- NOT SURE THE POS
+            if (data.Notes.includes("-------------------")) {
+                function extractProductInfo(line) {
+                    const regex = /(\d+(\.\d+)?) x ([^\d]+)([\d.]*(?: Gram|g)?(?: Pre-Roll)?)/i;
+                    const match = line.match(regex);
+                    if (match) {
+                        const quantity = Math.ceil(parseFloat(match[1]));
+                        const product = match[3].trim().replace(/-+$/, '').trim();
+                        const option = match[4].trim();
+                        return { quantity, product, option };
+                    }
+                    return null;
+                }
+
+                // Function to extract Total
+                function extractTotal(lines) {
+                    const totalLine = lines.find(line => line.includes('Total:'));
+                    const total = parseFloat(totalLine.match(/\$([\d.]+)/)[1]);
+                    return total;
+                }
+
+                // Split the input string into lines
+                const lines1 = data.Notes.split('\n');
+
+                // Find the start and end of the "Order Details" section
+                const start1 = lines1.indexOf('-------------------\nOrder Details\n-------------------') + 2;
+                const end1 = lines1.indexOf('Total:', start1);
+
+                // Extract product information and totals
+                const productInfo1 = lines1.slice(start1, end1).map(extractProductInfo).filter(Boolean);
+                const total1 = extractTotal(lines1);
+
+
+                // Output results
+                console.log("Product Info from input1:", productInfo1);
+                console.log("Total from input1:", total1);
+                dataObj.pos_system_used = "not sure2"
+                dataObj.order = productInfo1
+                dataObj.total = total1.total
+            }
+
             results.push(dataObj)
-            //   } 
-
-            // console.log(formattedData)
         })
         .on('end', () => {
-            console.log(results);
+
             res.json(results)
-            // [
-            //   { NAME: 'Daffy Duck', AGE: '24' },
-            //   { NAME: 'Bugs Bunny', AGE: '22' }
-            // ]
         });
-    //     fs.readFile("./contacts.csv","utf8", function (err, fileData) {
-    //      parse(fileData, {columns: false, trim: true}, function(err, rows) {
-    //    console.log(fileData)
-    // console.log(fileData)
-    // })
-    // })
-    //   Order.find({})
-    //     .then((dbNote) => {
-    //       res.json(dbNote);
-    //     })
-    //     .catch((err) => {
-    //       res.json(err);
-    //     });
 });
 
 app.put('/update/:id', ({ params, body }, res) => {
