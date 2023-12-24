730dec7d1369b0f1353eb6658b4a81f05474fac0
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2023-12-23 17:40:10 -0700

    added to post

--- server.js
+++ server.js
@@ -898,7 +898,7 @@ app.post('/testingroute/api/createtask', async (req, res) => {
     var info = await JSON.stringify(req)
     console.log(info)
     console.log("=================================================================")
-    res.sendJSON({a:1,b:2})
+    res.status(200).send(req.query.check)
 })
 
 app.get('/testingroute/webhook/createtask', async (req, res) => {
